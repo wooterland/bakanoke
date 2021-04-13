f51d39306987493170d0e00629ff2acc65548088
Vladislav Selivanov <wooterland@fastmail.com>
2021-04-14 01:02:47 +0200

    Story 4: Cleaning some text

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-url = "https://www.mangaupdates.com/series.html?id=1"
+
+url = "https://www.mangaupdates.com/series.html?id=177979"
 r = requests.get(url).content
 
 soup = BeautifulSoup(r, 'html.parser')
+cleaner = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+
 
 desc = soup.find("b", text="Description").find_next("div").string
 
@@ -21,40 +25,29 @@ complete_scan = soup.find("b", text="Completely Scanlated?").find_next("div").st
 
 anime_status = soup.find("b", text="Anime Start/End Chapter").find_next("div").string
 
-genre = soup.find("b", text="Genre").find_next("div").get_text("<u>").split("<u>")  # TODO Разобраться с лишними элементами в списке
+genre = soup.find("b", text="Genre").find_next("div").get_text("<u>").split("<u>")
+for i in range(1, len(genre)):
+    del genre[i]
+    if i == len(genre):
+        break
+del genre[-1]
 
-authors = soup.find("b", text="Author(s)").find_next("div")  # TODO Разобраться с сылками
 
-artists = soup.find("b", text="Artist(s)").find_next("div")  # TODO Разобраться с сылками
+authors = soup.find("b", text="Author(s)").find_next("div").get_text("<u>").split("<u>")
+print(authors)
+
+artists = soup.find("b", text="Artist(s)").find_next("div").get_text("<u>").split("<u>")
+del artists[-1]
 
 year = soup.find("b", text="Year").find_next("div").string
 
-pub = soup.find("b", text="Original Publisher").find_next("div")
+pub = soup.find("b", text="Original Publisher").find_next("div").get_text("<u>")
+pub = cleaner.sub('', pub)
 
-serialized = soup.find("b", text="Serialized In (magazine)").find_next("div")
+serialized = soup.find("b", text="Serialized In (magazine)").find_next("div").get_text("<u>")
+serialized = cleaner.sub('', serialized)
 
 licensed = soup.find("b", text="Licensed (in English)").find_next("div").string
 
-eng_pub = soup.find("b", text="English Publisher").find_next("div")
-
-print(genre)
-'''
-print(desc)
-print(manga_type)
-print(rel_series)
-print(ass_names)
-print(g_scan)
-print(last_release)
-print(status)
-print(comp_scan)
-print(anime_status)
-print(genre)
-print(authors)
-print(artists)
-print(year)
-print(pub)
-print(serialized)
-print(licensed)
-print(eng_pub)
-'''
-
+eng_pub = soup.find("b", text="English Publisher").find_next("div").get_text("<u>")
+eng_pub = cleaner.sub('', eng_pub)
